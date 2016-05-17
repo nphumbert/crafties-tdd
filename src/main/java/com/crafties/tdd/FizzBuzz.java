2e31349e8fdf1dd64b4f98c4047ef1f4cff61749
Nadia Humbert-Labeaumaz <np.humbert@gmail.com>
2016-05-17 12:26:36 +0200

    refactor(*): Add constant for fizz

--- src/main/java/com/crafties/tdd/FizzBuzz.java
+++ src/main/java/com/crafties/tdd/FizzBuzz.java
@@ -2,10 +2,12 @@ package com.crafties.tdd;
 
 public class FizzBuzz {
 
+    private static final String FIZZ = "fizz";
+
     public String display(int number) {
 
         if (number % 3 == 0) {
-            return "fizz";
+            return FIZZ;
         }
 
         return Integer.toString(number);
